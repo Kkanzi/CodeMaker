1988395a499bd24e2eadf00e3c8cfbec7ed7ca41
강지훈 <catp11@naver.com>
2022-11-04 22:18:19 +0900

    .

---
+++ KJHCore/AppConfigHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KJHCore
+{
+    public class AppConfigHelper
+    {
+        public static string GetAppConfig(string key) => ConfigurationManager.AppSettings[key];
+
+        public static void SetAppConfig(string key, string value)
+        {
+            System.Configuration.Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            KeyValueConfigurationCollection settings = configuration.AppSettings.Settings;
+            settings.Remove(key);
+            settings.Add(key, value);
+            configuration.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(configuration.AppSettings.SectionInformation.Name);
+        }
+
+        public static void AddAppConfig(string key, string value)
+        {
+            System.Configuration.Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            configuration.AppSettings.Settings.Add(key, value);
+            configuration.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(configuration.AppSettings.SectionInformation.Name);
+        }
+
+        public static void RemoveAppConfig(string key)
+        {
+            System.Configuration.Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            KeyValueConfigurationCollection settings = configuration.AppSettings.Settings;
+            try
+            {
+                settings.Remove(key);
+                configuration.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configuration.AppSettings.SectionInformation.Name);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
